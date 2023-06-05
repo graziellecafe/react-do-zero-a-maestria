8ffa1d97a91ddf51f29ef87e04b28b5a1c5f2456
Grazielle Café <graziellecafe@MacBook-Pro-de-Grazielle.local>
2023-06-05 17:14:10 -0300

    feat: seção sobre fundamentos de react, como criacao do componente e comentarios

---
+++ 2-fundamentos-do-react/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,15 @@
+// Fundamentos 
+
+const FirstComponent = () => { 
+//essa função chama o componnete 'meu primeiro componente '
+    return (
+        <div>
+            { /* Algum comentário */ }
+            <h1>
+                Meu primeiro componente
+            </h1>
+        </div>
+    );
+};
+
+export default FirstComponent;
